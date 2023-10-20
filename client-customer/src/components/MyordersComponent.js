1450caa9c0d48d70c4850b908cb0b6d49ad1665d
khannghoang <hok74554@gmail.com>
2023-10-20 09:53:11 +0700

    Update MyordersComponent.js

--- client-customer/src/components/MyordersComponent.js
+++ client-customer/src/components/MyordersComponent.js
@@ -43,15 +43,26 @@ class Myorders extends Component {
     }
     const full={
       borderRadius: "15px",
-      borderTopRightRadius:"0px",
       borderBottomRightRadius:"0px",
+      borderBottomLeftRadius:"0px",
       padding: "20px",
       backgroundColor: "white",
       width: "1060px",
       margin: "0 auto",
       boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.1)",
-      marginTop:"20px",
+      marginTop:"25px",
       height:"370px"
+    }
+    const full2={
+      borderRadius: "15px",
+      borderTopRightRadius:"0px",
+      borderTopLeftRadius:"0px",
+      padding: "20px",
+      backgroundColor: "white",
+      width: "1060px",
+      margin: "0 auto",
+      boxShadow: "0px 0px 10px 2px rgba(0,0,0,0.1)",
+      height:"250px"
     }
     const table={
       border:" 1px solid #fff",
@@ -83,9 +94,9 @@ class Myorders extends Component {
           </table>
         </div>
         {this.state.order ?
-          <div className="align-center">
-            <h2 className="text-center">ORDER DETAIL</h2>
-            <table className="datatable" border="1">
+          <div  style={full2} className="align-center">
+            <h2  style={move} className="text-center">ORDER DETAIL</h2>
+            <table style={table} className="datatable" border="1">
               <tbody>
                 <tr className="datatable">
                   <th>No.</th>
