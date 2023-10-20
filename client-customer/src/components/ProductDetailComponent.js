820916d0c7c8eca73bf33b2911f6d660017c5330
khannghoang <hok74554@gmail.com>
2023-10-20 10:03:38 +0700

    Update ProductDetailComponent.js

--- client-customer/src/components/ProductDetailComponent.js
+++ client-customer/src/components/ProductDetailComponent.js
@@ -29,7 +29,17 @@ class ProductDetail extends Component {
       color:"red"
 
     }
-   
+    const buttonStyle = {
+      borderRadius: "5px",
+      margin: "10px 0",
+      padding: "10px",
+      backgroundColor: "rgb(118, 74, 188)",
+      color: "white",
+      border: "none",
+      width: "55%", // Giảm chiều rộng để chia sẻ không gian với ô tìm kiếm
+      cursor: "pointer",
+      textAlign: "center",
+    };
     const prod = this.state.product;
     if (prod != null) {
       return (
@@ -63,7 +73,7 @@ class ProductDetail extends Component {
                     </tr>
                     <tr >
                       <td></td>
-                      <td><input className='but' type="submit" value="ADD TO CART" onClick={(e) => this.btnAdd2CartClick(e)}   /></td>
+                      <td><input style={buttonStyle} className='but' type="submit" value="ADD TO CART" onClick={(e) => this.btnAdd2CartClick(e)}   /></td>
                     </tr>
                   </tbody>
                 </table>
