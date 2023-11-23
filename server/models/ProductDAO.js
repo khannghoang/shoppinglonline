3ec1fd181bf2e98abea3c85a7d85cfaf8b20fb22
khannghoang <hok74554@gmail.com>
2023-11-23 11:04:20 +0700

    Update ProductDAO.js

--- server/models/ProductDAO.js
+++ server/models/ProductDAO.js
@@ -23,7 +23,7 @@ const ProductDAO = {
     return product;
   },
   async update(product) {
-    const newvalues = { name: product.name, price: product.price, image: product.image, category: product.category };
+    const newvalues = { name: product.name, price: product.price,description: product.description, image: product.image, category: product.category };
     const result = await Models.Product.findByIdAndUpdate(product._id, newvalues, { new: true });
     return result;
   },
