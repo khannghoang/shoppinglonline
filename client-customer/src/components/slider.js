650b5640daf7539390ae7f6875eeb7deabeb1dd1
khannghoang <hok74554@gmail.com>
2023-11-24 15:53:08 +0700

    Update slider.js

--- client-customer/src/components/slider.js
+++ client-customer/src/components/slider.js
@@ -21,21 +21,21 @@ function MySlider() {
       <Slider {...settings}>
         <div className='banner'>
           <img
-            src="https://i.pinimg.com/originals/8e/f7/26/8ef726ffe903afa19aa545e23f3b9c72.png"
+            src="https://i.imgur.com/wpLAn1y.png"
             alt="Image 1"
             className='imageStyle'
           />
         </div>
         <div className='banner'>
           <img
-            src="https://cdn.shopify.com/s/files/1/1043/3082/files/Web_banner_CNY.jpg?v=1641868757"
+            src="https://i.imgur.com/5ytUAC3.png"
             alt="Image 2"
             className='imageStyle'
           />
         </div>
         <div className='banner'>
           <img
-            src="https://insite.browntextbook.com/SiteImages/110-SchoolImages/110-TechCenter/110-MacBook%20Air%20M2%20(Jul22)%20Hero%20Space.png"
+            src="https://i.imgur.com/qQPHZ8G.png"
             alt="Image 3"
             className='imageStyle'
           />
