f85e4234885bc4e5215e77166c357652a9db82b5
khannghoang <hok74554@gmail.com>
2023-11-26 01:03:24 +0700

    Update ProductComponent.js

--- client-customer/src/components/ProductComponent.js
+++ client-customer/src/components/ProductComponent.js
@@ -25,29 +25,43 @@ class Product extends Component {
     const marginfor2={
       // marginLeft:"150px"
     }
+    const margin={
+      marginTop:"80px"
+    }
    
-   
+ 
    
     const prods = this.state.products.map((item) => {
       return (
-        <div key={item._id} className="inline fullboxlistproduct  scroll-animation">
-          <figure className='margincomponent'>
-            <Link to={'/product/' + item._id}><img className='imagelistproduct' src={"data:image/jpg;base64," + item.image} width="250px" height="250px" alt="" /></Link>
-            <figcaption style={textcente} ><div className='spanrespon truncate-text' >{item.name}</div><br /><span className="spanpricerespon truncate-text" >Price: {item.price}</span></figcaption>
+        <div key={item._id} className="inline fullbox scroll-animation">
+          <figure style={margin}>
+            <Link to={'/product/' + item._id}><img className='imagerespon' src={"data:image/jpg;base64," + item.image} width="250px" height="250px" alt="" /></Link>
+            <figcaption style={textcente} ><div className='spanrespon truncate-text' >{item.name}</div><br /><span className="spanpricerespon truncate-text" >{item.price} đ</span></figcaption>
           </figure>
         </div>
       );
     });
-    return (
-      <div style={marginfor} className="text-center text-center-respon scroll-animation" >
-        <h2 style={marginfor2} className="text-center">LIST PRODUCTS</h2>
-        <div className='wrapper'>
-        {prods}
-        </div>
+    // return (
+    //   <div style={marginfor} className="text-center text-center-respon scroll-animation" >
+    //     <h2 style={marginfor2} className="text-center">LIST PRODUCTS</h2>
+    //     <div className='wrapper'>
+    //     {prods}
+    //     </div>
         
+    //   </div>
+    // );
+    return (
+      <div  className="text-center text-center-respon scroll-animation" >
+        <h2  className="text-center">DANH SÁCH SẢN PHẨM</h2>
+        <div className='pp'>
+        {this.state.products.length > 0 ? prods :
+          <p>KHÔNG TÌM THẤY BẤT KỲ KẾT QUẢ NÀO VỚI TỪ KHÓA TRÊN.</p>
+        }
+        </div>
       </div>
     );
   }
+
   componentDidMount() { // first: /product/...
     const params = this.props.params;
     if (params.cid) {
