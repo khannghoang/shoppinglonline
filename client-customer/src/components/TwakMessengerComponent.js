dc7ae278c1a256e15a891638cc7b806413a886bb
khannghoang <hok74554@gmail.com>
2023-11-26 01:00:55 +0700

    Create TwakMessengerComponent.js

---
+++ client-customer/src/components/TwakMessengerComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+
+class TawkMessenger extends Component {
+  render() {
+    return (
+      <TawkMessengerReact propertyId='656172f4ba9fcf18a80eb4dd' widgetId='1hg2ai331' />
+    );
+  }
+}
+export default TawkMessenger;
